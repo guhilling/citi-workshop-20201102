5103dba4e67f74078c86aed16d1f1a5f3174d675
Lars Bohnsack <lbohnsac@redhat.com>
2020-02-11 11:21:08 +0000

    Update 0100-services.lab.example.com-client-installer.sh to version 4.3.0

--- 4.3.0-DEV/files/installations-scripts/services.fX.rhaw.io/0100-services.lab.example.com-client-installer.sh
+++ 4.3.0-DEV/files/installations-scripts/services.fX.rhaw.io/0100-services.lab.example.com-client-installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.2.14/openshift-install-linux-4.2.14.tar.gz
-wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.2.14/openshift-client-linux-4.2.14.tar.gz
-tar -xvf openshift-install-linux-4.2.14.tar.gz
-tar -xvf openshift-client-linux-4.2.14.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.3.0/openshift-install-linux-4.3.0.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.3.0/openshift-client-linux-4.3.0.tar.gz
+tar -xvf openshift-install-linux-4.3.0.tar.gz
+tar -xvf openshift-client-linux-4.3.0.tar.gz
 cp -v {oc,kubectl,openshift-install} /usr/bin/
-rm -Rf openshift-install-linux-4.2.14.tar.gz
-rm -Rf openshift-client-linux-4.2.14.tar.gz
+rm -Rf openshift-install-linux-4.3.0.tar.gz
+rm -Rf openshift-client-linux-4.3.0.tar.gz
 rm -Rf oc
 rm -Rf kubectl
 rm -Rf openshift-install
