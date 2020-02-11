89ea44a2e3129ebd3cdcb36dfa9c91b8b7efec1f
Lars Bohnsack <lbohnsac@redhat.com>
2020-02-11 11:19:21 +0000

    Update 0080-services.lab.example.com-ocp-images.sh to version 4.3.0

--- 4.3.0-DEV/files/installations-scripts/services.fX.rhaw.io/0080-services.lab.example.com-ocp-images.sh
+++ 4.3.0-DEV/files/installations-scripts/services.fX.rhaw.io/0080-services.lab.example.com-ocp-images.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p /var/lib/tftpboot/openshift4/4.2.0/
-cd /var/lib/tftpboot/openshift4/4.2.0/
-wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.2/latest/rhcos-4.2.0-x86_64-installer-kernel
-wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.2/latest/rhcos-4.2.0-x86_64-installer-initramfs.img
-restorecon -RFv /var/lib/tftpboot/openshift4/4.2.0/
-mkdir -p /var/www/html/openshift4/4.2.0/images/
-cd  /var/www/html/openshift4/4.2.0/images/
-wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.2/latest/rhcos-4.2.0-x86_64-metal-bios.raw.gz
+mkdir -p /var/lib/tftpboot/openshift4/4.3.0/
+cd /var/lib/tftpboot/openshift4/4.3.0/
+wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.3/4.3.0/rhcos-4.3.0-x86_64-installer-kernel
+wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.3/4.3.0/rhcos-4.3.0-x86_64-installer-initramfs.img
+restorecon -RFv /var/lib/tftpboot/openshift4/4.3.0/
+mkdir -p /var/www/html/openshift4/4.3.0/images/
+cd  /var/www/html/openshift4/4.3.0/images/
+wget https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.3/4.3.0/rhcos-4.3.0-x86_64-metal-bios.raw.gz
 restorecon -RFv /var/www/html/
-mkdir /var/www/html/openshift4/4.2.0/ignitions
+mkdir /var/www/html/openshift4/4.3.0/ignitions
